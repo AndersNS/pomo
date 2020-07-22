f05109724d4b82fd4d29b8bf3039c93a558f57a6
AndersNS <anders.slinde@gmail.com>
2020-07-22 19:36:50 +0200

    Added button component

---
+++ components/Button/Button.tsx
@@ -0,0 +1,25 @@
+interface Props {
+  text: string;
+  color: 'primary' | 'secondary';
+  size?: 'large' | 'normal';
+  rounded?: boolean;
+  onClick: Function;
+}
+export default function Button({
+  text,
+  color: style,
+  size,
+  rounded,
+  onClick,
+}: Props) {
+  const s = size ? size : 'normal';
+  const r = rounded ? 'is-rounded' : '';
+  return (
+    <button
+      className={`button is-${style} is-${s} ${r}`}
+      onClick={() => onClick()}
+    >
+      {text}
+    </button>
+  );
+}
