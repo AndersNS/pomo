cb39d7354b0d9613ce21938e474c0d9ffad8ed0f
AndersNS <anders.slinde@gmail.com>
2020-07-22 19:35:21 +0200

    Made content fill the page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,21 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <AppHead />
-      <Component {...pageProps} />
+      <div className="wrapper">
+        <main className="site-content">
+          <Component {...pageProps} />
+        </main>
+      </div>
+      <style jsx>{`
+        .wrapper {
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+        }
+        .site-content {
+          flex: 1;
+        }
+      `}</style>
     </>
   );
 }
